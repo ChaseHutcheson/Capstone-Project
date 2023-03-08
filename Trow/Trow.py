81e76de8bbb429df5ce63972efc1bf2cb5d4c7c5
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2023-03-08 01:34:38 -0500

    Got pytess working

--- Trow/Trow.py
+++ Trow/Trow.py
@@ -4,6 +4,10 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from googletrans import Translator
+import pytesseract as pytess
+pytess.pytesseract.tesseract_cmd = r'Trow\\Tesseract-OCR\\tesseract.exe'
+from PIL import Image
+import cv2
 import time
 import datetime
 Builder.load_file("Trow.kv")
@@ -16,8 +20,10 @@ class PhotoTranslate(Screen):
     def capture(self):
         camera = self.ids['cam']
         timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
+        camera.export_to_png(f"{timestr}.png")
+        img = Image.open(f"{timestr}.png")
+        translation = pytess.image_to_string(img)
+        self.ids.photo_untranslated.text = f"{translation}"
 
 
 class TextTranslate(Screen):
